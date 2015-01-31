9df371dc2e3f0c9b3c4ae2d533e205a62d264b15
npu3paka <cheetahbg93@yahoo.com>
2015-01-31 16:24:30 +0200

    Add User Model.

---
+++ Near/Classes/Models/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  Near
+//
+//  Created by 158ltd Developer on 1/31/15.
+//  Copyright (c) 2015 158ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ Near/Classes/Models/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  Near
+//
+//  Created by 158ltd Developer on 1/31/15.
+//  Copyright (c) 2015 158ltd. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
