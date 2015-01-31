871d634cf72977041a8fc7754b25efa31b7b88d6
npu3paka <cheetahbg93@yahoo.com>
2015-01-31 16:04:01 +0200

    Make new structure for the project.

---
+++ Near/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Near
+//
+//  Created by 158ltd Developer on 1/31/15.
+//  Copyright (c) 2015 158ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
