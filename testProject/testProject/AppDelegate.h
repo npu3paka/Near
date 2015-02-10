d46ad5631114ffe4fac16fb209cfa0c281a536f2
npu3paka <cheetahbg93@yahoo.com>
2015-02-10 19:22:13 +0200

    second

---
+++ testProject/testProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProject
+//
+//  Created by Altimir Antonov on 2/10/15.
+//  Copyright (c) 2015 Altimir Antonov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testProject/testProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testProject
+//
+//  Created by Altimir Antonov on 2/10/15.
+//  Copyright (c) 2015 Altimir Antonov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
